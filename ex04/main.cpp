e6d886de7e45e5affb013de362e8f5b5ad3ad4ab
Kana Hashimoto <kanahash@c1r2s1.42tokyo.jp>
2025-07-20 23:31:24 +0900

    add ex04

--- ex04/main.cpp
+++ ex04/main.cpp
@@ -6,7 +6,7 @@
 /*   By: kanahash <kanahash@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/19 23:30:47 by kanahash          #+#    #+#             */
-/*   Updated: 2025/07/19 23:30:57 by kanahash         ###   ########.fr       */
+/*   Updated: 2025/07/20 23:20:17 by kanahash         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,15 +14,20 @@
 #include <iostream>
 #include <fstream>
 
-int open_file(std::string Receive_inputfile, std::string Receive_outputfile, std::ifstream *inputfile, std::ofstream *outputfile)
+int open_file(std::string& Receive_inputfile, std::string& Receive_outputfile, std::ifstream &inputfile, std::ofstream &outputfile)
 {
-	(*inputfile).open(Receive_inputfile.c_str(), std::fstream::in);
-	(*outputfile).open(Receive_outputfile.c_str(), std::fstream::out);
-	if(!inputfile || !outputfile)
+	inputfile.open(Receive_inputfile.c_str(), std::fstream::in);
+
+	if(!inputfile.is_open())
 	{
-		std::cerr << "Failed to open files!!!" << std::endl;
-		(*inputfile).close();
-		(*outputfile).close();
+		std::cerr << "Error: Could not open input file." << std::endl;
+		return(1);
+	}
+	outputfile.open(Receive_outputfile.c_str(), std::fstream::out);
+	if(!outputfile.is_open())
+	{
+		std::cerr << "Error: Could not create output file." << std::endl;
+		inputfile.close();
 		return(1);
 	}
 	return(0);
@@ -48,7 +53,7 @@ std::string replace(const std::string& line, const std::string& to_find, const s
 	return(replaced_line);
 }
 
-void read_and_replace(char **av, std::ifstream *inputfile, std::ofstream *outputfile)
+void read_and_replace(char **av, std::ifstream &inputfile, std::ofstream &outputfile)
 {
 	std::string to_find;
 	std::string to_replace;
@@ -58,14 +63,14 @@ void read_and_replace(char **av, std::ifstream *inputfile, std::ofstream *output
 	to_find = *(av + 2);
 	to_replace = *(av + 3);
 
-	while(std::getline(*inputfile, line))
+	while(std::getline(inputfile, line))
 	{
 		replaced_line = replace(line, to_find, to_replace);
 
-		if(!(*inputfile).eof())
-			*outputfile << replaced_line << std::endl;
+		if(!inputfile.eof())
+			outputfile << replaced_line << std::endl;
 		else
-			*outputfile << replaced_line;
+			outputfile << replaced_line;
 	}
 }
 
@@ -77,12 +82,15 @@ int main(int ac, char **av)
 	std::ofstream outputfile;
 
 	if(ac != 4)
-		return(std::cout << "Wrong number of arguments" << std::endl, 0);
+	{
+		std::cerr << "Usage: ./Sed_is_for_losers <filename> <s1> <s2>" << std::endl;
+		return(1);
+	}
 	Receive_inputfile = av[1];
 	Receive_outputfile = Receive_inputfile + ".replace";
-	if(open_file(Receive_inputfile, Receive_outputfile, &inputfile, &outputfile))
+	if(open_file(Receive_inputfile, Receive_outputfile, inputfile, outputfile))
 		return(1);
-	read_and_replace(av, &inputfile, &outputfile);
+	read_and_replace(av, inputfile, outputfile);
 	inputfile.close();
 	outputfile.close();
 	return(0);
